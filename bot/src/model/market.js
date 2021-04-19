b9f6cf1d775dc45f1a9df03a4a3b3cefff8fc638
PanosMits <panag.mitseas@gmail.com>
2021-04-19 23:41:56 +0100

    Add symbol to Market

--- bot/src/model/market.js
+++ bot/src/model/market.js
@@ -8,10 +8,19 @@ class Market {
 
     spot;
 
-    constructor(base, quote, spot) {
+    symbol;
+
+    /**
+     * @param {string} base The base currency of the market
+     * @param {string} quote The quote currency of the market
+     * @param {boolean} spot Whether the market is availbe on spot trading or not
+     * @param {string} symbol The market's symbol. It's always in the format of base/quote
+     */
+    constructor(base, quote, spot, symbol) {
         this.base = base;
         this.quote = quote;
         this.spot = spot;
+        this.symbol = symbol;
     }
 
     /**
@@ -20,7 +29,12 @@ class Market {
      * @returns {Market} The market instance
      */
     static fromResponse(marketConfig) {
-        return new this(marketConfig.base, marketConfig.quote, marketConfig.spot);
+        return new this(
+            marketConfig.base,
+            marketConfig.quote,
+            marketConfig.spot,
+            marketConfig.symbol
+        );
     }
 
     /**

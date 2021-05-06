56dbbf218d4784fab548df45a7a8f70eea8ffeda
PanosMits <panag.mitseas@gmail.com>
2021-05-06 15:31:07 +0100

    Saving to work on DB Migration

--- bot/src/bot/binance-bot.js
+++ bot/src/bot/binance-bot.js
@@ -1,5 +1,7 @@
 const MarketsService = require('../services/markets-service');
 const CandlestickService = require('../services/candlestick-service');
+const TickerService = require('../services/ticker-service');
+const OrderService = require('../services/order-service');
 
 class Bot {
     /**
@@ -13,25 +15,64 @@ class Bot {
     #candlestickService;
 
     /**
+     *  @param {TickerService}
+     */
+    #tickerService;
+
+    /**
+     *  @param {OrderService}
+     */
+    #orderService;
+
+    /**
      * @param {MarketsService} marketsService The markets service
      * @param {CandlestickService} candlestickService The candlestick service
+     * @param {TickerkService} tickerService The ticker service
+     * @param {OrderService} orderService The order service
      */
-    constructor(marketsService, candlestickService) {
+    constructor(marketsService, candlestickService, tickerService, orderService) {
         this.#marketsService = marketsService;
         this.#candlestickService = candlestickService;
+        this.#tickerService = tickerService;
+        this.#orderService = orderService;
     }
 
     async run() {
-        const dollarMarketsCollection = await this.#marketsService.getDollarMarkets();
-        console.log(dollarMarketsCollection);
+        // WORKING
+        // const dollarMarketsCollection = await this.#marketsService.getDollarMarkets();
+        // const marketCandlestickResults = await this.#candlestickService.get5mCandlesticks(dollarMarketsCollection);
+        // marketCandlestickResults.map((marketCandlestickResult) => {
+        //     console.log(`24 hour difference for: ${marketCandlestickResult.toObject().symbol} is: ${marketCandlestickResult.getMarket24HourDifference()}%`);
+        // });
 
-        const ohlcv = await this.#candlestickService.getCandlestickForMarkets(dollarMarketsCollection);
-        console.log(ohlcv);
-        
-        // Get the candlestic data for each market
-        // await this.#candleStickService.getMinute
-        // const ohlc = await exchange.fetchOHLCV(dollarMarket.symbol, '1m', undefined, 1000); // Get last 1000 minutes ohlc
+
+        // TEST getTicker
+        // const dollarMarketsCollection = await this.#marketsService.getDollarMarkets();
+        // Promise.all(dollarMarketsCollection.toArray().map(async (market) => {
+        //     const result = await this.#tickerService.getTicker(market.symbol);
+        //     console.log(`24 hour difference for: ${market.symbol} is: ${result.percentage}%`);
+        // }));
+
+
+        // TEST getTickers - Fastest way of getting the 24 hour change
+        // const marketCollection = await this.#marketsService.getDollarMarketsExcludingETFs();
+        // const tickerCollection = await this.#tickerService.getTickersForMarkets(marketCollection);
+        // console.log(tickerCollection.getBiggestNegativePercentages());
+
+
+        // TEST create order
+        console.time('Time');
+        const marketCollection = await this.#marketsService.getDollarMarkets();
+        const tickerCollection = await this.#tickerService.getTickersForMarkets(marketCollection);
+        const tickers = tickerCollection.getBiggestNegativePercentages();
+
+        if (tickers) {
+            Promise.all(tickers.map((ticker) => this.#orderService.createMarketBuyOrder(ticker.symbol)))
+                .then(() => console.timeEnd('Time'));
+        }
+
+        console.log(tickers);
     }
 }
 
-module.exports = new Bot(MarketsService, CandlestickService);
+module.exports = new Bot(MarketsService, CandlestickService, TickerService, OrderService);
